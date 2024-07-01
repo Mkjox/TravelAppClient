a1f74e8b6ec30cbc0c3bc847c4298f7f63ef0bc1
jox <zincir_890@hotmail.com>
2024-07-01 19:10:15 +0300

    Added new colors

--- src/assets/colors/colors.js
+++ src/assets/colors/colors.js
@@ -6,7 +6,10 @@ const colors = {
     white: '#FBFCFE',
     blue: '#3CB2D8',
     tealdark: '#009688',
-    teallight: '#4DB6AC'
+    teallight: '#4DB6AC',
+    coffee: '#C67C4E',
+    skin: '#EDD9C8',
+    lightBlack: '#302F2F'
 };
 
 export default colors;
