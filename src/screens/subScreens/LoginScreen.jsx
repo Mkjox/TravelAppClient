5a523383bf59f607c574c875d8999859c7831003
jox <zincir_890@hotmail.com>
2024-07-04 07:29:33 +0300

    Designed Login Screen

--- src/screens/subScreens/LoginScreen.jsx
+++ src/screens/subScreens/LoginScreen.jsx
@@ -1,23 +1,146 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from "@react-navigation/core";
+import colors from '../../assets/colors/colors';
 
 const LoginScreen = () => {
+    const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if (!email || !password) {
+            Alert.alert('Error', 'Please fill all fields');
+            return;
+        }
+
+        // const apiUrl = '';
+
+        const data = {
+            email,
+            password
+        };
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                if (result.success) {
+                    Alert.alert('Success', 'Registration is successful');
+                }
+                else {
+                    Alert.alert('Error', result.message || 'Registration failed');
+                }
+            })
+            .catch((error) => {
+                Alert.alert('Error', 'An error occured');
+                console.error(error);
+            });
+    };
+
+
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Text>
-                    This is Login Screen
+                <Text style={styles.title}>
+                    Welcome Back!
                 </Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Enter your email'
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Enter your password'
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+
+                {/* Add navigation to the forgot password page */}
+                <TouchableOpacity onPress={() => ''}>
+                    <Text style={styles.forgotPassword}>
+                        Forgot Password?
+                    </Text>
+                </TouchableOpacity>
             </View>
+
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                <Text style={styles.buttonText}>
+                    Login
+                </Text>
+            </TouchableOpacity>
+
+            <Text style={styles.signUpText}>
+                Don't have an account? {<Text style={styles.signUpLink} onPress={navigation.navigate('Register')}>Sign Up</Text>}
+            </Text>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
-    }
+        flex: 1,
+        backgroundColor: '#EEEEEE',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontFamily: 'Poppins_700Bold',
+        alignSelf: 'center',
+        marginVertical: 15,
+        fontSize: 24
+    },
+    textInput: {
+        marginHorizontal: 10,
+        marginTop: 15,
+        alignSelf: 'center',
+        color: colors.teallight,
+        width: 300,
+        backgroundColor: colors.white,
+        height: 65,
+        textAlign: 'center',
+        borderRadius: 25,
+        fontFamily: 'Poppins_400Regular'
+    },
+    forgotPassword: {
+        color: '#007BFF',
+        alignSelf: 'center',
+        marginBottom: 20,
+        marginTop: 15
+    },
+    button: {
+        width: 300,
+        margin: 10,
+        alignSelf: 'center',
+        borderRadius: 15,
+        padding: 20,
+        bottom: -170,
+        backgroundColor: colors.teallight,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+    },
+    signUpText: {
+        fontSize: 14,
+        color: '#7D7D7D',
+        bottom: -180,
+    },
+    signUpLink: {
+        color: '#007BFF',
+        fontWeight: 'bold',
+    },
 });
 
 export default LoginScreen;
