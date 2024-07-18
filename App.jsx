ca934afebae1cd157180d3e87cef1639e5b85c33
jox <zincir_890@hotmail.com>
2024-07-18 09:59:37 +0300

    Built better navigation and font system

--- App.jsx
+++ App.jsx
@@ -1,21 +1,5 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import ProfileScreen from "./src/screens/ProfileScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import React, { useEffect } from "react";
 import "react-native-gesture-handler";
-import HomeScreen from "./src/screens/HomeScreen";
-import LikedScreen from "./src/screens/LikedScreen";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import AppSettings from "./src/components/AppSettings";
-import PostDetails from "./src/screens/subScreens/PostDetails";
-import SettingsScreen from "./src/screens/SettingsScreen";
-import DonateScreen from "./src/screens/subScreens/DonateScreen";
-import Post from "./src/components/Post";
-import Explore from "./src/components/Explore";
-import AddPostScreen from "./src/screens/subScreens/AddPostScreen";
 import {
   useFonts,
   Poppins_100Thin,
@@ -37,185 +21,13 @@ import {
   Poppins_900Black,
   Poppins_900Black_Italic,
 } from "@expo-google-fonts/poppins";
-import SharedPostsScreen from "./src/screens/subScreens/SharedPostsScreen";
-import CommentsSreen from "./src/screens/subScreens/CommentsScreen";
-import CustomizeProfileScreen from "./src/screens/subScreens/CustomizeProfileScreen";
-import NotificationsScreen from "./src/screens/subScreens/NotificationsScreen";
-import PrivacyScreen from "./src/screens/subScreens/PrivacyScreen";
-import LanguageScreen from "./src/screens/subScreens/LanguageScreen";
-import StartPage from "./src/screens/subScreens/StartPage";
-import RegisterScreen from "./src/screens/subScreens/RegisterScreen";
-import LoginScreen from "./src/screens/subScreens/LoginScreen";
+import * as SplashScreen from 'expo-splash-screen';
+import AppNavigator from "./src/navigation/AppNavigator";
 
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
+SplashScreen.preventAutoHideAsync();
 
-
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: "#e91e63",
-        tabBarStyle: { borderTopStartRadius: 15, borderTopEndRadius: 15 },
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-          headerShown: false,
-        }}
-      />
-
-      <Tab.Screen
-        name="Liked"
-        component={LikedScreen}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="heart" color={color} size={size} />
-          ),
-          headerShown: false,
-        }}
-      />
-
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-          headerShown: false,
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
-
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={StackNavigator} options={{ drawerType: 'back', headerShown: false }} />
-      <Drawer.Screen name="AppSettings" component={AppSettings} options={{ drawerType: 'back', headerShown: false }} />
-    </Drawer.Navigator>
-  );
-}
-
-function StackNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="TabNavigator"
-        component={TabNavigator}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="PostDetails"
-        component={PostDetails}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="AppSettings"
-        component={AppSettings}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Donate"
-        component={DonateScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Post"
-        component={Post}
-        options={styles.header} />
-
-      <Stack.Screen
-        name="Explore"
-        component={Explore}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="AddPost"
-        component={AddPostScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="SharedPosts"
-        component={SharedPostsScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Comments"
-        component={CommentsSreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="CustomizeProfile"
-        component={CustomizeProfileScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Notifications"
-        component={NotificationsScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Language"
-        component={LanguageScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Privacy"
-        component={PrivacyScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Start"
-        component={StartPage}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Register"
-        component={RegisterScreen}
-        options={styles.header}
-      />
-
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={styles.header}
-      />
-
-    </Stack.Navigator>
-  );
-}
-
-const App = () => {
-  let [fontsLoaded] = useFonts({
+export default function App() {
+  const [loaded, error] = useFonts({
     Poppins_100Thin,
     Poppins_100Thin_Italic,
     Poppins_200ExtraLight,
@@ -235,32 +47,16 @@ const App = () => {
     Poppins_900Black,
     Poppins_900Black_Italic,
   });
-  return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
-  );
+
+  useEffect(() => {
+  if (loaded|| error){
+    SplashScreen.hideAsync();
+  }
+}, [loaded,error]);
+
+if (!loaded && !error) {
+  return null;
+}
+
+  return <AppNavigator/>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  main: {
-    // flex: 1,
-    // textAlign: 'center',
-    // alignContent: 'center',
-    // marginTop: 50,
-    // margin: 50,
-    // alignSelf: 'center',
-    // display: 'flex'
-  },
-  header: {
-    headerShown: false,
-  },
-});
-
-export default App;
---
+++ src/navigation/AppNavigator.jsx
@@ -0,0 +1,185 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import ProfileScreen from "../screens/ProfileScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import HomeScreen from "../screens/HomeScreen";
+import LikedScreen from "../screens/LikedScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import AppSettings from "../components/AppSettings";
+import PostDetails from "../screens/subScreens/PostDetails";
+import SettingsScreen from "../screens/SettingsScreen";
+import DonateScreen from "../screens/subScreens/DonateScreen";
+import Post from "../components/Post";
+import Explore from "../components/Explore";
+import AddPostScreen from "../screens/subScreens/AddPostScreen";
+import SharedPostsScreen from "../screens/subScreens/SharedPostsScreen";
+import CommentsSreen from "../screens/subScreens/CommentsScreen";
+import CustomizeProfileScreen from "../screens/subScreens/CustomizeProfileScreen";
+import NotificationsScreen from "../screens/subScreens/NotificationsScreen";
+import PrivacyScreen from "../screens/subScreens/PrivacyScreen";
+import LanguageScreen from "../screens/subScreens/LanguageScreen";
+import StartPage from "../screens/subScreens/StartPage";
+import RegisterScreen from "../screens/subScreens/RegisterScreen";
+import LoginScreen from "../screens/subScreens/LoginScreen";
+import colors from "../assets/colors/colors";
+import CustomTabBar from "../components/CustomTabBar";
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+function TabNavigator() {
+    return (
+        <Tab.Navigator initialRouteName="Home" tabBar={(props) => <CustomTabBar {...props} />}>
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    headerShown: false,
+                }} />
+
+            <Tab.Screen
+                name="Liked"
+                component={LikedScreen}
+                options={{
+                    headerShown: false,
+                }} />
+
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    headerShown: false,
+                }} />
+        </Tab.Navigator>
+    );
+}
+
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={StackNavigator} options={{ drawerType: 'back', headerShown: false }} />
+            <Drawer.Screen name="AppSettings" component={AppSettings} options={{ drawerType: 'back', headerShown: false }} />
+        </Drawer.Navigator>
+    );
+}
+
+function StackNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="TabNavigator"
+                component={TabNavigator}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="PostDetails"
+                component={PostDetails}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="AppSettings"
+                component={AppSettings}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Settings"
+                component={SettingsScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Donate"
+                component={DonateScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Post"
+                component={Post}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Explore"
+                component={Explore}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="AddPost"
+                component={AddPostScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="SharedPosts"
+                component={SharedPostsScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Comments"
+                component={CommentsSreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="CustomizeProfile"
+                component={CustomizeProfileScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Notifications"
+                component={NotificationsScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Language"
+                component={LanguageScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Privacy"
+                component={PrivacyScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Start"
+                component={StartPage}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Register"
+                component={RegisterScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <DrawerNavigator />
+        </NavigationContainer>
+    );
+}
